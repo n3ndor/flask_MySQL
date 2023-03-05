270fbfba30102b0d9bd32904149f10dced6be164
n3ndor <n3ndor@gmail.com>
2023-03-05 10:35:31 -0300

    added bootstrap for the 4 pages

--- crud/books/flask_app/controllers/authors_route.py
+++ crud/books/flask_app/controllers/authors_route.py
@@ -3,4 +3,12 @@ from flask_app import app
 
 @app.route('/')
 def index():
+    return redirect("/authors")
+
+@app.route("/authors")
+def authors():
     return render_template("authors.html")
+
+@app.route("/author")
+def author():
+    return render_template("author_show.html")
--- crud/books/flask_app/controllers/books_route.py
+++ crud/books/flask_app/controllers/books_route.py
@@ -2,5 +2,9 @@ from flask import render_template, redirect, request
 from flask_app import app
 
 @app.route('/books')
-def index():
+def books():
     return render_template("books.html")
+
+@app.route('/book')
+def book():
+    return render_template("book_show.html")
