d083d41be46f2f35f7f15ed350b635e1f1a81910
n3ndor <n3ndor@gmail.com>
2023-03-06 14:51:58 -0300

    init files

---
+++ crud/friendships/flask_app/controllers/u_route.py
@@ -0,0 +1,7 @@
+from flask import render_template, redirect, request
+from flask_app import app
+from flask_app.models.u_class import User
+
+@app.route("/")
+def index():
+    return render_template("index.html")
---
+++ crud/friendships/flask_app/models/u_class.py
@@ -0,0 +1,10 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+
+class User:
+    DB= "friendships_schema"
+    def __init__(self, data):
+        self.id = data["id"]
+        self.first_name = data["first_name"]
+        self.last_name = data["last_name"]
+        self.created_at = data["created_at"]
+        self.updated_at = data["updated_at"]
